dea21155164a8b73c918d06bd2a65fb8b7384627
JimmyChen <0984166132a@gmail.com>
2026-04-06 11:45:15 +0800

    Upload Lab2 - Uart

---
+++ Lab_2/Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/FreeRTOS.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/projdefs.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/portable.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/deprecated_definitions.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/mpu_wrappers.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/list.h
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/FreeRTOS.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/FreeRTOSConfig.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/projdefs.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/portable.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/deprecated_definitions.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/mpu_wrappers.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_2/FreeRTOS/include/list.h:
