1ebcab54837dbd649fbdfa0d16f31454e9ecdea2
JimmyChen <0984166132a@gmail.com>
2026-05-03 16:15:44 +0800

    Upload Lab4 - Memory Management

---
+++ Lab_4/Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/FreeRTOS.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/projdefs.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/portable.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/deprecated_definitions.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/mpu_wrappers.h \
+ /Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/list.h
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/FreeRTOS.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/FreeRTOSConfig.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/projdefs.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/portable.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/deprecated_definitions.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/mpu_wrappers.h:
+/Users/chenguanming/Desktop/Course/Embedded/Lab_4/FreeRTOS/include/list.h:
